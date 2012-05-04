263ffabdc7622e4b8e59e93c02fab0d35864b672
Tool Management System Administrator <toolman@toolman.toolman.gacmotor.com>
2012-05-04 08:20:07 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toolman::Application.config.secret_token = 'e08a06002cf39d0064947cac2b6b68a96513d914924fa9abc3b08926740a732ff12c092c26c740cdc268bac0aea6dc0784963121378b96735a321dbde8c5c41b'
