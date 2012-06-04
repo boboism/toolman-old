717784c27b1fb5795bd6d16c7ded4632d89dcb2a
Jianbo Su <sujianbo@gmail.com>
2012-06-04 22:17:20 +0800

    backup

--- app/controllers/barcode_generators_controller.rb
+++ app/controllers/barcode_generators_controller.rb
@@ -18,16 +18,14 @@ class BarcodeGeneratorsController < ApplicationController
       file = params[:archive][:archive]
       @barcodes = {}
       root = Rails.root.join('public','tmp')
-      if file
-        CSV.foreach(file.tempfile) do |row|
-          if row
-            barcode = Barby::GS1128.new(row[0],'B', '').to_png(:height => 38, :margin => 0)
-            path = "#{(Time.now.to_f*10000000).to_i}.png"
-            file = File.open(root.join(path), 'w') {|f| f.write barcode}
-            @barcodes[row[0]] = "/tmp/#{path}"
-          end
-        end
-      end
+      Dir::mkdir(root) unless FileTest.directory? root
+      CSV.foreach(file.tempfile) do |row|
+        next unless row
+        barcode = Barby::GS1128.new(row[0],'B', '').to_png(:height => 38, :margin => 0)
+        path = "#{(Time.now.to_f*10000000).to_i}.png"
+        File.open(root.join(path), 'w') {|f| f.write barcode}
+        @barcodes[row[0]] = "/tmp/#{path}"
+      end if file
       render :index, :layout => "pdf" and return
     else
       render :new, :notice => 'not csv file' and return
