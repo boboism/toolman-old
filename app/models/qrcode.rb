07b768e61dbe8027f72d9fde428f340429b5bc11
Jianbo Su <sujianbo@gmail.com>
2012-06-27 09:30:23 +0800

    backup

--- app/models/qrcode.rb
+++ app/models/qrcode.rb
@@ -1,6 +1,6 @@
 class Qrcode < Archive
   has_attached_file :archive,
-                    :url => "/assets/qrcodes/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/qrcodes/:id/:style/:basename.:extension"
+                    :url => "/tmp/qrcodes/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/tmp/qrcodes/:id/:style/:basename.:extension"
   attr_accessible :archive_file_name, :archive_content_type, :archive_file_size, :archive_updated_at, :archivable_id, :archivable_type
 end
